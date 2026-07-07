a741db6196a0bfece979496086da54cb2dcd894a
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 20:35:27 +0530

    Update video routes.js
    lists the 
    web addresses related to videos and says which controller function should run for each one .
    it doesn't do the actual work it is just points to the worker

--- server/routes/video routes.js
+++ server/routes/video routes.js
@@ -1,1 +1,22 @@
 //list all the url addresses to videos 
+
+
+
+const express = require("express");
+const router = express.Router();
+const {
+  getAllVideos,
+  getVideoById,
+  uploadVideo,
+} = require("../controllers/videoController");
+
+// GET all videos
+router.get("/", getAllVideos);
+
+// GET one video by id
+router.get("/:id", getVideoById);
+
+// POST (upload) a new video
+router.post("/", uploadVideo);
+
+module.exports = router;
