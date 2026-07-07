8cd658a21e853f989006e01b7c48c9bb2406e6f5
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 20:38:10 +0530

    Update index.js
    this is the on switch 
    it starts the server connects to the database and tells the app 
    any request to api,videos to video routes any request to api,users go to user routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,35 @@
 //this file will start the backend server 
+
+// Import tools
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Import routes
+const videoRoutes = require("./routes/videoRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+// Create the server
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Allow frontend to talk to backend + understand JSON
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+// Use our routes
+app.use("/api/videos", videoRoutes);
+app.use("/api/users", userRoutes);
+
+// Start server
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
