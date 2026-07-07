0cdab931e5023d64e80aaae775d023135e2608a1
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 20:22:37 +0530

    Update videocontroller.js
    the real logic get all videos fetches every video from the database get video by id fetches just one . upload the video and saves a new video someone submits

--- server/controllers/videocontroller.js
+++ server/controllers/videocontroller.js
@@ -1,1 +1,36 @@
 //contains the logic for fetching/adding/deleting videos 
+
+const Video = require("../models/Video");
+
+// Get all videos
+const getAllVideos = async (req, res) => {
+  try {
+    const videos = await Video.find();
+    res.json(videos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get one video by id
+const getVideoById = async (req, res) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    res.json(video);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Upload a new video
+const uploadVideo = async (req, res) => {
+  try {
+    const newVideo = new Video(req.body);
+    const savedVideo = await newVideo.save();
+    res.status(201).json(savedVideo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAllVideos, getVideoById, uploadVideo };
