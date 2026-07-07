34ce9d22c31a2f3594bb463d5111b6c60ae9a93b
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 20:16:40 +0530

    Update db.js
    connects your server to our MongoDB database using  a secret link (MONGO_URI, which we will store safely later, not typed directly here )

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,14 @@
 //this file connects the server to MongoDB
+
+ const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.log("MongoDB connection failed:", error.message);
+  }
+};
+
+module.exports = connectDB;
