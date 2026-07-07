9e79b0f9d648269c369ede55ed0d396c548b00f2
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 20:33:14 +0530

    Update user routes.js
    list the  
    login 
    register
    web addresses 
    points them to right controller function

--- server/routes/user routes.js
+++ server/routes/user routes.js
@@ -1,1 +1,13 @@
 //list all the url addresses to login/register 
+
+const express = require("express");
+const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/userController");
+
+// Register a new user
+router.post("/register", registerUser);
+
+// Login existing user
+router.post("/login", loginUser);
+
+module.exports = router;
