1672b0d908ffc7bfa91b68bb1cd47bc1859cdf5e
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 19:58:10 +0530

    Fetch and display videos in Home component
    when the page loads , it asks the backend to give all the videos (axios.get) stores the answer in videos and it shows a video card for each one

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,1 +1,26 @@
 
+import { useEffect, useState } from "react";
+import axios from "axios";
+import VideoCard from "../components/VideoCard";
+
+function Home() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get("http://localhost:5000/api/videos");
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", padding: "20px" }}>
+      {videos.map((video) => (
+        <VideoCard key={video._id} video={video} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
