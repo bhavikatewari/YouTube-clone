e8a1742bd4b437fe0ed22cce1bc727479f090368
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 20:30:03 +0530

    Update Video.js
    defines what a video looks like when saved in the data base 
    title 
    description 
    video link 
    thumbnail 
    who uploaded 
    likes 
    views 
    timestamps true automatically records when it was created

--- server/models/Video.js
+++ server/models/Video.js
@@ -1,1 +1,18 @@
 //defines the structure of a video (title, url ,likes)
+
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    videoUrl: { type: String, required: true },
+    thumbnailUrl: { type: String },
+    uploader: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    likes: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Video", videoSchema);
