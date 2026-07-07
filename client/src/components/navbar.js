a447be71b24ffae513a249db9109870edb363b11
bhavikatewari <bhavikatewari@gmail.com>
2026-07-07 19:54:19 +0530

    Update navbar.js
    the bar shown on every page 
    logo on the left 
    login link on the right

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,1 +1,18 @@
 //the top navigation bar shown on every page 
+
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px 20px", background: "#181818" }}>
+      <Link to="/" style={{ color: "white", fontSize: "20px", textDecoration: "none" }}>
+        🎬 MyTube
+      </Link>
+      <Link to="/login" style={{ color: "white", textDecoration: "none" }}>
+        Login
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
