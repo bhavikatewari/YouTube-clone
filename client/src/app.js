ec2f6300dc8fd87e65ce5c312ad81a3a9eadacd9
bhavikatewari <bhavikatewari@gmail.com>
2026-07-09 21:25:33 +0530

    Add API_URL constant and fix route casing
    Added API_URL constant for use in Home.js and VideoCard.

--- client/src/app.js
+++ client/src/app.js
@@ -5,6 +5,8 @@ import Home from "./pages/Home";
 import VideoPage from "./pages/VideoPage";
 import LoginPage from "./pages/LoginPage";
 import UploadPage from "./pages/uploadPage";
+const API_URL = "https://youtube-clone-1h0h.onrender.com";
+export { API_URL }; // ye isliye taki Home.js aur VideoCard me use ho jaye
 
 function App() {
   return (
@@ -14,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/video/:id" element={<VideoPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <route path="/Upload" element={<UploadPage/>}/>
+        <Route path="/Upload" element={<UploadPage/>}/>
       </Routes>
     </BrowserRouter>
   );
