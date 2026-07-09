dc54fe1d65bc7f4ca061ada05baf352540f56c0d
bhavikatewari <bhavikatewari@gmail.com>
2026-07-09 17:08:28 +0530

    Remove commented-out connectDB function

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const userRoutes = require("./routes/userRoutes");
 // Create the server
 const app = express();
 
-//const connectDB = async () => {
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
